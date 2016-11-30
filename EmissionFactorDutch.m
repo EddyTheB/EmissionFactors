91747b57bb6a7a24a2ae33de046a8de678fd58d2
EddyTheB <eddybarratt@gmail.com>
2016-11-30 16:52:33 +0000

    Now reads the appropriate values direct from the spreadsheet for non-motorway traffic

--- EmissionFactorDutch.m
+++ EmissionFactorDutch.m
@@ -7,15 +7,17 @@ function F = EmissionFactorDutch(varargin)
     %
     % USAGE
     % F = EmissionFactorDutch
-    % F = EmissionFactorDutch(Year)
-    % F = EmissionFactorDutch(Year, Pollutant)
-    % F = EmissionFactorDutch(Year, Pollutant, VehicleClass)
-    % F = EmissionFactorDutch(Year, Pollutant, VehicleClass, SpeedClass)
-    % 
+    % F = EmissionFactorDutch(Pollutant)
+    % F = EmissionFactorDutch(Pollutant, VehicleClass)
+    % F = EmissionFactorDutch(Pollutant, VehicleClass, SpeedClass)
+    % F = EmissionFactorDutch(Pollutant, VehicleClass, SpeedClass)
+    % F = EmissionFactorDutch( ... '-Year', year)
+    % F = EmissionFactorDutch( ... '-SourceFile', filename)
+    % F = EmissionFactorDutch( ... 'Motorway')
     %
-    % Factors where taken from the published spreadsheet downloaded in
-    % September 2015. The spreadsheet is uploaded annualy and can be
-    % downloaded from http://tinyurl.com/EF-NonMotorway.
+    % Factors are read from spreadsheets downloaded from the Dutch
+    % government. As of November 2015 the following links worked: 
+    % http://tinyurl.com/EF-NonMotorway and http://tinyurl.com/EF-Motorway 
     %
     % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % $Workfile:   EmissionFactorDutch.m  $
@@ -23,84 +25,185 @@ function F = EmissionFactorDutch(varargin)
     % $Author:   edward.barratt  $
     % $Date:   Nov 24 2016 09:19:14  $
     % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        
-    Factors.PM10.Light.Stagnated = 0.0437;
-    Factors.PM10.Light.Normal = 0.0399;
-    Factors.PM10.Light.Smooth = 0.0401;
-    Factors.PM10.Light.LargeRoad = 0.0212;
-    Factors.PM10.Medium.Stagnated = 0.2478;
-    Factors.PM10.Medium.Normal = 0.1930;
-    Factors.PM10.Medium.Smooth = 0.1664;
-    Factors.PM10.Medium.LargeRoad = 0.1116;
-    Factors.PM10.Heavy.Stagnated = 0.2669;
-    Factors.PM10.Heavy.Normal = 0.2025;
-    Factors.PM10.Heavy.Smooth = 0.1713;
-    Factors.PM10.Heavy.LargeRoad = 0.1129;
-    Factors.PM10.Bus.Stagnated = 0.3828;
-    Factors.PM10.Bus.Normal = 0.2443;
-    Factors.PM10.Bus.Smooth = 0.1825;
-    Factors.PM10.Bus.LargeRoad = 0.1638;
-
-    Factors.PM25.Light.Stagnated = 0.0224;
-    Factors.PM25.Light.Normal = 0.0186;
-    Factors.PM25.Light.Smooth = 0.0188;
-    Factors.PM25.Light.LargeRoad = 0.0102;
-    Factors.PM25.Medium.Stagnated = 0.1461;
-    Factors.PM25.Medium.Normal = 0.0913;
-    Factors.PM25.Medium.Smooth = 0.0646;
-    Factors.PM25.Medium.LargeRoad = 0.0560;
-    Factors.PM25.Heavy.Stagnated = 0.1718;
-    Factors.PM25.Heavy.Normal = 0.1074;
-    Factors.PM25.Heavy.Smooth = 0.0762;
-    Factors.PM25.Heavy.LargeRoad = 0.0618;
-    Factors.PM25.Bus.Stagnated = 0.3054;
-    Factors.PM25.Bus.Normal = 0.1668;
-    Factors.PM25.Bus.Smooth = 0.1050;
-    Factors.PM25.Bus.LargeRoad = 0.1214;
-
-    Factors.NO2.Light.Stagnated = 0.1500;
-    Factors.NO2.Light.Normal = 0.0900;
-    Factors.NO2.Light.Smooth = 0.0900;
-    Factors.NO2.Light.LargeRoad = 0.0800;
-    Factors.NO2.Medium.Stagnated = 0.7300;
-    Factors.NO2.Medium.Normal = 0.4400;
-    Factors.NO2.Medium.Smooth = 0.2900;
-    Factors.NO2.Medium.LargeRoad = 0.2800;
-    Factors.NO2.Heavy.Stagnated = 0.7900;
-    Factors.NO2.Heavy.Normal = 0.4800;
-    Factors.NO2.Heavy.Smooth = 0.3200;
-    Factors.NO2.Heavy.LargeRoad = 0.3000;
-    Factors.NO2.Bus.Stagnated = 1.0400;
-    Factors.NO2.Bus.Normal = 0.6500;
-    Factors.NO2.Bus.Smooth = 0.4700;
-    Factors.NO2.Bus.LargeRoad = 0.4400;
-
-    Factors.NOx.Light.Stagnated = 0.6100;
-    Factors.NOx.Light.Normal = 0.3800;
-    Factors.NOx.Light.Smooth = 0.3900;
-    Factors.NOx.Light.LargeRoad = 0.2700;
-    Factors.NOx.Medium.Stagnated = 12.500;
-    Factors.NOx.Medium.Normal = 7.6000;
-    Factors.NOx.Medium.Smooth = 5.2000;
-    Factors.NOx.Medium.LargeRoad = 4.8000;
-    Factors.NOx.Heavy.Stagnated = 16.600;
-    Factors.NOx.Heavy.Normal = 10.100;
-    Factors.NOx.Heavy.Smooth = 7.0000;
-    Factors.NOx.Heavy.LargeRoad = 5.6000;
-    Factors.NOx.Bus.Stagnated = 10.300;
-    Factors.NOx.Bus.Normal = 6.4000;
-    Factors.NOx.Bus.Smooth = 4.6000;
-    Factors.NOx.Bus.LargeRoad = 4.1000;
+      
+    NonMotorwayPath = [fileparts(which('EmissionFactorDutch')), '\Data\DutchEF-NonMotorway.xlsx'];
+    MotorwayPath = [fileparts(which('EmissionFactorDutch')), '\Data\DutchEF-Motorway.xlsx'];
+    [year, ~] = datevec(now);
     
-    if nargin == 0
-        F = Factors;
-    elseif nargin == 1
-        F = Factors.(varargin{1});
-    elseif nargin == 2
-        F = Factors.(varargin{1}).(varargin{2});
-    elseif nargin > 2
-        F = Factors.(varargin{1}).(varargin{2}).(varargin{3});
+    [SFB, SFi] = ismember('-SourceFile', varargin);
+    if SFB
+        SourceFile = varargin{SFi+1};
+        varargin(SFi+1) = [];
+        varargin(SFi) = [];
+    elseif ismember('Motorway', varargin)
+        SourceFile = MotorwayPath;
+        varargin(end) = [];
+    else
+        SourceFile = NonMotorwayPath;
     end
+    [yB, yi] = ismember('-Year', varargin);
+    if yB
+        year = varargin{yi+1};
+        if ~isequal(year, 'all')
+            year_ = str2double(year);
+            if isnan(year_)
+                error('EmissionFactorsDutch:UnrecognizedYear', 'Year ''%s'' is not understood.', year)
+            else
+                year = year_;
+            end
+        end
+        varargin(yi+1) = [];
+        varargin(yi) = [];
+    end
+    
+    % Read the source file.
+    [~, ~, raw] = xlsread(SourceFile);
+    
+    if isequal(raw{1,1}, 'Emissiefactoren voor niet-snelwegen (SRM1)')
+        MWay = 0;
+    elseif isequal(raw{1,1}, 'Emissiefactoren voor snelwegen (SRM2)')
+        %MWay = 1;
+        error('Not ready yet')
+    else
+        error('Source file structure is not recognised.')
+    end
+    
+    [NumRows, ~] = size(raw);
+    
+    if ~MWay
+        VehiclesToSearch = {'LICHT WEGVERKEER', 'MIDDELZWAAR WEGVERKEER', ...
+                            'ZWAAR WEGVERKEER', 'AUTOBUSSEN'};
+        Vehicles = {'Light', 'Medium', 'Heavy', 'Bus'};
+        PollutantsToSearch = {'NOx in NO2-equivalenten (g/km)', ...
+                             'NO2 (g/km)', ...
+                             'PM10 verbranding + slijtage naar lucht (g/km)', ...
+                             'PM2.5 verbranding + slijtage naar lucht (g/km)', ...
+                             'CO (g/km)', ...
+                             'B(a)P verbranding (µg/km)', ...
+                             'benzeen (mg/km) verbranding + verdamping', ...
+                             'SO2 (mg/km)'};
+        Pollutants = {'NOx', 'NO2', 'PM10', 'PM25', 'CO', 'BaP', 'Benzeen', 'SO2'};
+        PScales = [0, 0, 0, 0, 0, -6, -3, -3]; % Scales to convert to g/km
+        PYearExtra = [3, 3, 3, 3, 3, 4, 4, 4];
+        SpeedClasses = {'Stagnated', 'Normal', 'Smooth', 'LargeRoad'};
+        SpeedClassColNums = 1:4;
+
+        
+        % Find the column numbers for the different vehicle classes.
+        VehColNums = nan(1, numel(Vehicles));
+        for vi = 1:numel(Vehicles)
+            index = find(strcmp(raw, VehiclesToSearch{vi}));
+            VehColNums(vi) = floor(index/NumRows) + 1;
+        end
+        
+        % Find the row numbers for the different pollutants.
+        PolRowNums = nan(1, numel(Pollutants));
+        for pi = 1:numel(Pollutants)
+            index = find(strcmp(raw, PollutantsToSearch{pi}));
+            PolRowNums(pi) = index(1);
+        end
+
+        % Find the available Years.
+        Years = raw(PolRowNums(1)+1:PolRowNums(2)-1, 1);
+        Years = [Years{:}];
+        Years = Years(isfinite(Years));
+        YearsRowNums = (1:numel(Years));
+        if ~isequal(year, 'all')
+            [yB, yi] = ismember(year, Years);
+            if ~yB
+                error('EmissionFactorsDutch:UnknownYear', 'Year ''%d'' is not known.', year)
+            end
+            Years = year;
+            YearsRowNums = YearsRowNums(yi);
+        end
+
+        % See what factors have been requested.
+        NumVars = numel(varargin);
+        if NumVars > 0
+            % Pollutant Specified.
+            [Is, Pi] = ismember(varargin{1}, Pollutants);
+            if ~Is
+                error('EmissionFactorsDutch:UnknownPollutant', 'Pollutant ''%s'' is not known.', varargin{1})
+            end
+            Pollutants = Pollutants(Pi);
+            PScales = PScales(Pi);
+            PolRowNums = PolRowNums(Pi);
+            PYearExtra = PYearExtra(Pi);
+        end
+        if NumVars > 1
+            % Vehicle class specified.
+            [Is, Pi] = ismember(varargin{2}, Vehicles);
+            if ~Is
+                error('EmissionFactorsDutch:UnknownVehicleClass', 'Vehicle Class ''%s'' is not known', varargin{2})
+            end
+            Vehicles = Vehicles(Pi);
+            VehColNums = VehColNums(Pi);
+        end
+        if NumVars > 1
+            % Vehicle class specified.
+            [Is, Pi] = ismember(varargin{2}, Vehicles);
+            if ~Is
+                error('EmissionFactorsDutch:UnknownVehicleClass', 'Vehicle Class ''%s'' is not known', varargin{2})
+            end
+            Vehicles = varargin(2);
+            VehColNums = VehColNums(Pi);
+        end
+        if NumVars > 2
+            % speed class specified.
+            [Is, Pi] = ismember(varargin{3}, SpeedClasses);
+            if ~Is
+                error('EmissionFactorsDutch:UnknownSpeedClass', 'Speed Class ''%s'' is not known', varargin{3})
+            end
+            SpeedClasses = varargin(3);
+            SpeedClassColNums = SpeedClassColNums(Pi);
+        end
+        
+        % Populate the structure.
+        F = struct;
+        for Yi = 1:numel(Years)
+            Y = Years(Yi);
+            YRow = YearsRowNums(Yi);
+            F.(sprintf('Y_%d', Y)) = struct;
+            for Pi = 1:numel(Pollutants)
+                P = Pollutants{Pi};
+                PRowStart = PolRowNums(Pi);
+                PRowExtra = PYearExtra(Pi);
+                PScale = PScales(Pi);
+                F.(sprintf('Y_%d', Y)).(P) = struct;
+                for Vi = 1:numel(Vehicles)
+                    V = Vehicles{Vi};
+                    VColStart = VehColNums(Vi);
+                    F.(sprintf('Y_%d', Y)).(P).(V) = struct;
+                    for Si = 1:numel(SpeedClasses)
+                        S = SpeedClasses{Si};
+                        SCol = SpeedClassColNums(Si);
+                        Row = PRowStart + YRow + PRowExtra;
+                        Col = VColStart + SCol;
+                        %fprintf('%s %s %s %d: %dx%d = %f\n', P, V, S, Y, Row, Col, raw{Row, Col})
+                        F.(sprintf('Y_%d', Y)).(P).(V).(S) = raw{Row, Col} * 10^PScale;
+                    end
+                end
+            end
+        end
+    end
+    
+    
+    % That's the structure produced, now simplify it.
+    FFNs = fieldnames(F);
+    while numel(FFNs) == 1
+        F = F.(FFNs{1});
+        try
+            FFNs = fieldnames(F);
+        catch E
+            if ~isequal(E.identifier, 'MATLAB:UndefinedFunction')
+                disp(E)
+                rethrow(E)
+            end
+            break
+        end
+    end
+    
+ 
 end
 
 
